8fadce8e3e8f5e94b5424d0921db03ac83069533
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-10-19 07:13:46 +0100

    Update ror-workflow config
    - Install Bundles
    - Create the new App
    - Install and Configure Rspec
    - Install and Configure Cucumber
    - Set the Environment to Development
    - Install Devise, plus generate the Views and the Migration
    - Create and Migrate the Database
    - Copy config files into their paths

--- ror-workflow.sh
+++ ror-workflow.sh
@@ -13,46 +13,118 @@ echo "
 
 if [ ! -z "$1" ]; then
 
+    # Check if the folder already created
+    # If so, remove it
     sudo rm -r $1
+
+    # Create and Enter to the Folder of the new app
     mkdir $1
     cd $1
-    cp ../templates/* .
+
+    # Copy the Gemfile and .gitignore to the application folder
+    cp ../templates/Gemfile ../templates/.gitignore .
+
+    # Install Bundles
     bundle install
+
+    # Create the New APP
     rails new . -d mysql --webpack --skip-git --skip-gemfile --skip-test
+
+    # Set the Environment to development
     bin/rails db:environment:set RAILS_ENV=development
+
+    # Install Rspec
     rails generate rspec:install
+
+    # Install Cucumber
+    rails generate cucumber:install
+
+    # Install Devise, plus generate the Views and the Migration
     rails generate devise:install
     rails generate devise:views
     rails generate devise User
-    bin/rails db:drop db:create db:migrate RAILS_ENV=developmen
+
+    # Create and Migrate the Database
+    bin/rails db:drop db:create db:migrate RAILS_ENV=development
     rails generate controller home index --no-helper --no-assets --no-controller-specs --no-view-specs
 
     #
     #
-    # Devise Config
-    echo "\n\n1 + Add these two lines to config/routes.rb"
+    #
+    #
+    #
+    # Devise Config Files
+    echo "\n\n1 + Add these two lines to 'config/routes.rb'"
     echo "root to: 'home#index'"
     echo "devise_for :users\n"
     #
     echo "2 + Add this line to 'config/environments/development.rb' "
     echo "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }\n"
     #
-    echo "3 + Optional: Add this line to application_controller.rb"
+    echo "3 + Optional: Add this line to 'app/controllers/application_controller.rb'"
     echo "before_action :authenticate_user! \n"
-    #
-    echo "4 + "
 
+    ################### Stop
     read -p "next step? (y/n) " -n 1
     echo "Good Bye"
      
     #
     # If Devise is configured
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo "Yep"    
+
+      # Rspec Config File
+      echo  "'rails_helper.rb' will be modified with the new version"
+      cp ../templates/spec/rails_helper.rb spec/
+
+      # Factory Girl
+      cp ../templates/spec/factories/users.rb spec/factories/
+
+      # Guard
+      bundle exec guard init rspec
+
+      #
+      #
+      #
+      #
+      #
+      # Webpack Config Files
+      echo "4 + Remove this line form 'views/layouts/application.html.erb'"
+      echo "<%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>\n"
+
+      #
+      echo "5 + And replace it with theses two lines on the <head></head> section"
+      echo "
+      <meta name=\"viewport\" content=\"width=device-width, user-scalable=no\">
+      <%= csrf_meta_tags %>
+      <%= javascript_pack_tag 'application' %>
+      <%= stylesheet_pack_tag 'application' %>"
+
+      # Install Yarn Dependencies
+      cp ../templates/package.json .
+      yarn install
+
+      #
+      #
+      #
+      #
+      #
+      # Theme Files
+      cp ../templates/app/views/layouts/_notices.html.erb app/views/layouts
+
+      #
+      #
+      #
+      #
+      # The End
+      echo "7 + Run these commands on separate consoles"
+      echo "rails server"
+      echo "yarn dev_server"
+      echo "rails console"
+
     fi
 
 else
-    echo " + You need to set the app name 'example: ./run.sh app_name' \n"
+  echo " + You need to set the app name 'example: ./run.sh app_name' \n"
 
 fi
 
